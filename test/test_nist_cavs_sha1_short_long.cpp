8deb6dfbbfa4439cc111ea64be93d1ab0116ca8a
ckormanyos <ckormanyos@yahoo.com>
2024-10-21 15:10:56 +0200

    Try running sha1 Monte Carlo tests

--- test/test_nist_cavs_sha1_short_long.cpp
+++ test/test_nist_cavs_sha1_short_long.cpp
@@ -3,9 +3,9 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include "test_nist_cavs_detail.hpp"
-
 #include <boost/crypt/hash/sha1.hpp>
+
+#include "test_nist_cavs_detail.hpp"
 
 auto main() -> int
 {
