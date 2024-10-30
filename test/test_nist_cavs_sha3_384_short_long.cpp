07892464d7d50bec99116d3eba37e1e42a6af4c6
Matt Borland <matt@mattborland.com>
2024-10-30 10:37:25 -0400

    Add NIST SHA3VS testing

---
+++ test/test_nist_cavs_sha3_384_short_long.cpp
@@ -0,0 +1,36 @@
+// Copyright 2024 Matt Borland
+// Copyright 2024 Christopher Kormanyos
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/crypt/hash/sha3_384.hpp>
+
+#include "test_nist_cavs_detail.hpp"
+
+auto main() -> int
+{
+    bool result_is_ok { true };
+
+    {
+        nist::cavs::test_vector_container_type test_vectors_short { };
+
+        static_cast<void>(nist::cavs::detail::parse_file_vectors("SHA3_384ShortMsg.rsp", test_vectors_short));
+
+        result_is_ok = (nist::cavs::test_vectors_oneshot<boost::crypt::sha3_384_hasher>(test_vectors_short) && result_is_ok);
+
+        BOOST_TEST(result_is_ok);
+    }
+
+
+    {
+        nist::cavs::test_vector_container_type test_vectors_long { };
+
+        static_cast<void>(nist::cavs::detail::parse_file_vectors("SHA3_384LongMsg.rsp", test_vectors_long));
+
+        result_is_ok = (nist::cavs::test_vectors_oneshot<boost::crypt::sha3_384_hasher>(test_vectors_long) && result_is_ok);
+
+        BOOST_TEST(result_is_ok);
+    }
+
+    return boost::report_errors();
+}
