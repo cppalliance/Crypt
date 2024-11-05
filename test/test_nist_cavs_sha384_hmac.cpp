6d274388fbaa9713f0f327bea82581d6adfb64ec
Matt Borland <matt@mattborland.com>
2024-11-05 14:01:48 -0500

    Add SHA384 HMAC testing

---
+++ test/test_nist_cavs_sha384_hmac.cpp
@@ -0,0 +1,35 @@
+// Copyright 2024 Matt Borland
+// Copyright 2024 Christopher Kormanyos
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/crypt/hash/sha384.hpp>
+
+#include "test_nist_cavs_detail.hpp"
+
+auto main() -> int
+{
+    bool result_is_ok { true };
+
+    {
+        nist::cavs::test_vector_container_type test_vectors {};
+
+        static_cast<void>(nist::cavs::detail::parse_file_vectors_hmac("sha384.fax", test_vectors));
+
+        result_is_ok = (nist::cavs::test_vectors_hmac<boost::crypt::sha384_hasher>(test_vectors) && result_is_ok);
+
+        BOOST_TEST(result_is_ok);
+    }
+
+    {
+        nist::cavs::test_vector_container_type test_vectors {};
+
+        static_cast<void>(nist::cavs::detail::parse_file_vectors_hmac("sha384cavs11.fax", test_vectors));
+
+        result_is_ok = (nist::cavs::test_vectors_hmac<boost::crypt::sha384_hasher>(test_vectors) && result_is_ok);
+
+        BOOST_TEST(result_is_ok);
+    }
+
+    return boost::report_errors();
+}
