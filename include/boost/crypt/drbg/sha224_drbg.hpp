b3ba2f8018d0e37ad0db16367dedd09edeb25b0a
Matt Borland <matt@mattborland.com>
2024-11-12 15:03:27 -0500

    Add SHA224 hmac drbg definitions

---
+++ include/boost/crypt/drbg/sha224_drbg.hpp
@@ -0,0 +1,27 @@
+// Copyright 2024 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_CRYPT_DRBG_SHA1_DRBG_HPP
+#define BOOST_CRYPT_DRBG_SHA1_DRBG_HPP
+
+#include <boost/crypt/drbg/detail/hmac_drbg.hpp>
+#include <boost/crypt/hash/sha224.hpp>
+
+namespace boost {
+namespace crypt {
+
+namespace drbg {
+
+template <bool prediction_resistance>
+using sha224_hmac_drbg_t = drbg::hmac_drbg<hmac<sha224_hasher>, 192U, 224U, prediction_resistance>;
+
+} // namespace drbg
+
+BOOST_CRYPT_EXPORT using sha224_hmac_drbg = drbg::sha224_hmac_drbg_t<false>;
+BOOST_CRYPT_EXPORT using sha224_hmac_drbg_pr = drbg::sha224_hmac_drbg_t<true>;
+
+} // namespace crypt
+} // namespace boost
+
+#endif // BOOST_CRYPT_DRBG_SHA1_DRBG_HPP
