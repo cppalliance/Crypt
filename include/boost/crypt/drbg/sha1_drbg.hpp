160be225526f481566af4be3433c15c5306228ae
Matt Borland <matt@mattborland.com>
2024-11-18 15:48:33 -0500

    Add definition of SHA1 hash drbg

--- include/boost/crypt/drbg/sha1_drbg.hpp
+++ include/boost/crypt/drbg/sha1_drbg.hpp
@@ -6,6 +6,7 @@
 #define BOOST_CRYPT_DRBG_SHA1_DRBG_HPP
 
 #include <boost/crypt/drbg/detail/hmac_drbg.hpp>
+#include <boost/crypt/drbg/detail/hash_drbg.hpp>
 #include <boost/crypt/hash/sha1.hpp>
 
 namespace boost {
@@ -14,9 +15,15 @@ namespace crypt {
 namespace drbg {
 
 template <bool prediction_resistance>
-using sha1_hmac_drbg_t = drbg::hmac_drbg<hmac<sha1_hasher>, 128U, 160U, prediction_resistance>;
+using sha1_hash_drbg_t = hash_drbg<sha1_hasher, 128U, 160U, prediction_resistance>;
+
+template <bool prediction_resistance>
+using sha1_hmac_drbg_t = hmac_drbg<hmac<sha1_hasher>, 128U, 160U, prediction_resistance>;
 
 } // namespace drbg
+
+BOOST_CRYPT_EXPORT using sha1_hash_drbg = drbg::sha1_hash_drbg_t<false>;
+BOOST_CRYPT_EXPORT using sha1_hash_drbg_pr = drbg::sha1_hash_drbg_t<true>;
 
 BOOST_CRYPT_EXPORT using sha1_hmac_drbg = drbg::sha1_hmac_drbg_t<false>;
 BOOST_CRYPT_EXPORT using sha1_hmac_drbg_pr = drbg::sha1_hmac_drbg_t<true>;
