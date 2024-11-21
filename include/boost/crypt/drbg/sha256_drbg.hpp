cf4de478a6df2430503fbbbabf112bbe3305abcb
Matt Borland <matt@mattborland.com>
2024-11-21 11:24:49 -0500

    Add SHA256 hash drbg overloads

--- include/boost/crypt/drbg/sha256_drbg.hpp
+++ include/boost/crypt/drbg/sha256_drbg.hpp
@@ -6,6 +6,7 @@
 #define BOOST_CRYPT_DRBG_SHA256_DRBG_HPP
 
 #include <boost/crypt/drbg/detail/hmac_drbg.hpp>
+#include <boost/crypt/drbg/detail/hash_drbg.hpp>
 #include <boost/crypt/hash/sha256.hpp>
 
 namespace boost {
@@ -14,9 +15,15 @@ namespace crypt {
 namespace drbg {
 
 template <bool prediction_resistance>
+using sha256_hash_drbg_t = drbg::hash_drbg<sha256_hasher, 256U, 256U, prediction_resistance>;
+
+template <bool prediction_resistance>
 using sha256_hmac_drbg_t = drbg::hmac_drbg<hmac<sha256_hasher>, 256U, 256U, prediction_resistance>;
 
 } // namespace drbg
+
+BOOST_CRYPT_EXPORT using sha256_hash_drbg = drbg::sha256_hash_drbg_t<false>;
+BOOST_CRYPT_EXPORT using sha256_hash_drbg_pr = drbg::sha256_hash_drbg_t<true>;
 
 BOOST_CRYPT_EXPORT using sha256_hmac_drbg = drbg::sha256_hmac_drbg_t<false>;
 BOOST_CRYPT_EXPORT using sha256_hmac_drbg_pr = drbg::sha256_hmac_drbg_t<true>;
