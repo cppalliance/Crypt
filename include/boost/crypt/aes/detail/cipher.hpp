9d721dcac7cb113e18de2f99093ba8c1251f193f
Matt Borland <matt@mattborland.com>
2024-11-25 13:31:17 -0500

    Add the most basic of structure

---
+++ include/boost/crypt/aes/detail/cipher.hpp
@@ -0,0 +1,8 @@
+// Copyright 2024 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_CRYPT_AES_DETAIL_CIPHER_HPP
+#define BOOST_CRYPT_AES_DETAIL_CIPHER_HPP
+
+#endif // BOOST_CRYPT_AES_DETAIL_CIPHER_HPP
