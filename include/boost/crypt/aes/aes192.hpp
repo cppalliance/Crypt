dacee8fb045b160d2a7e58c8bd0f842a529d7acd
Matt Borland <matt@mattborland.com>
2024-12-04 08:26:10 -0500

    Add AES192 definition

---
+++ include/boost/crypt/aes/aes192.hpp
@@ -0,0 +1,20 @@
+// Copyright 2024 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_AES192_HPP
+#define BOOST_AES192_HPP
+
+#include <boost/crypt/utility/config.hpp>
+#include <boost/crypt/aes/detail/cipher.hpp>
+#include <boost/crypt/aes/detail/cipher_mode.hpp>
+
+namespace boost {
+namespace crypt {
+
+BOOST_CRYPT_EXPORT using aes192 = aes::cipher<12>;
+
+}
+}
+
+#endif //BOOST_AES192_HPP
