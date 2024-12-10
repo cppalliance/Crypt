551c9792e580b4401c19def0a823b43ce9d4f60d
Matt Borland <matt@mattborland.com>
2024-12-10 14:01:51 -0500

    Add test harness for CTR mode

--- test/test_nist_cavs_detail.hpp
+++ test/test_nist_cavs_detail.hpp
@@ -2154,9 +2154,55 @@ auto test_vectors_drbg_pr_true(const nist::cavs::test_vector_container_drbg_pr_t
 #  pragma warning( disable : 4127 ) // Conditional expression is constant (which is true before C++17 in BOOST_CRYPT_IF_CONSTEXPR)
 #endif
 
+namespace detail {
+
+// Since AESAVS does not provide any testing for CTR mode we will use the existing files
+// And make sure that we are able to roundtrip the plaintext
+template <typename AESType>
+auto test_vectors_aes_ctr(const nist::cavs::test_vector_container_aes &test_vectors) -> bool
+{
+    BOOST_TEST(!test_vectors.empty());
+
+    bool result_is_ok { true };
+
+    const auto total_tests {test_vectors.size()};
+    std::size_t count {};
+    for (const auto& test_vector : test_vectors)
+    {
+        auto plaintext {test_vector.plaintext};
+        const auto iv {test_vector.iv};
+
+        AESType aes;
+
+        aes.init(test_vector.key.begin(), test_vector.key.size());
+
+        aes.template encrypt<boost::crypt::aes::cipher_mode::ctr>(plaintext.begin(), plaintext.size(), iv.begin(), iv.size());
+        aes.template decrypt<boost::crypt::aes::cipher_mode::ctr>(plaintext.begin(), plaintext.size(), iv.begin(), iv.size());
+
+        if (plaintext != test_vector.plaintext)
+        {
+            // LCOV_EXCL_START
+            result_is_ok = false;
+            std::cerr << "Error with vector: " << count << std::endl;
+            // LCOV_EXCL_STOP
+        }
+
+        ++count;
+    }
+
+    return result_is_ok;
+}
+
+} // namespace detail
+
 template <boost::crypt::aes::cipher_mode mode, typename AESType>
 auto test_vectors_aes_kat(const nist::cavs::test_vector_container_aes& test_vectors) -> bool
 {
+    BOOST_CRYPT_IF_CONSTEXPR (mode == boost::crypt::aes::cipher_mode::ctr)
+    {
+        return detail::test_vectors_aes_ctr<AESType>(test_vectors);
+    }
+
     BOOST_TEST(!test_vectors.empty());
 
     bool result_is_ok { true };
@@ -2201,6 +2247,11 @@ auto test_vectors_aes_kat(const nist::cavs::test_vector_container_aes& test_vect
 template <boost::crypt::aes::cipher_mode mode, typename AESType>
 auto test_vectors_aes_mmt(const nist::cavs::test_vector_container_aes& test_vectors) -> bool
 {
+    BOOST_CRYPT_IF_CONSTEXPR (mode == boost::crypt::aes::cipher_mode::ctr)
+    {
+        return detail::test_vectors_aes_ctr<AESType>(test_vectors);
+    }
+
     BOOST_TEST(!test_vectors.empty());
 
     bool result_is_ok { true };
