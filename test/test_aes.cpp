17df7a411a9e1b7e9ace4bafd45dbdbcfd1fe1b6
Matt Borland <matt@mattborland.com>
2024-12-10 11:29:51 -0500

    Add trivial test

--- test/test_aes.cpp
+++ test/test_aes.cpp
@@ -173,6 +173,37 @@ void ofb_mmt_test()
     BOOST_TEST(plaintext == plaintext_original);
 }
 
+void ctr_test()
+{
+    // AES-128 key from appendix A.1
+    boost::crypt::array<uint8_t, 16> key = {
+            0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+            0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
+    };
+
+    boost::crypt::array<uint8_t, 16> plaintext = {
+            0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d,
+            0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34
+    };
+
+    constexpr boost::crypt::array<boost::crypt::uint8_t, 16> iv = {
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+
+    const auto original_message {plaintext};
+
+    boost::crypt::aes128 gen;
+
+    BOOST_TEST(gen.init(key, key.size()) == boost::crypt::state::success);
+    BOOST_TEST(gen.encrypt<boost::crypt::aes::cipher_mode::ctr>(plaintext.begin(), plaintext.size(), iv.begin(), iv.size()) == boost::crypt::state::success);
+    BOOST_TEST(gen.decrypt<boost::crypt::aes::cipher_mode::ctr>(plaintext.begin(), plaintext.size()) == boost::crypt::state::success);
+
+    BOOST_TEST(plaintext == original_message);
+
+    gen.destroy();
+}
+
 int main()
 {
     basic_aes128_test();
@@ -180,6 +211,7 @@ int main()
     cbc_mmt_test();
     ofb_test();
     ofb_mmt_test();
+    ctr_test();
 
     return boost::report_errors();
 }
