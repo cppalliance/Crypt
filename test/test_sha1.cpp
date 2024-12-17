4f1309a7168700fe3e42e4986bc5f8f373d0f928
Matt Borland <matt@mattborland.com>
2024-12-17 07:52:37 -0500

    Modern SHA1 testing

--- test/test_sha1.cpp
+++ test/test_sha1.cpp
@@ -2,27 +2,17 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/crypt/hash/sha1.hpp>
-#include <boost/core/lightweight_test.hpp>
-#include "generate_random_strings.hpp"
+#include <boost/crypt2/hash/sha1.hpp>
 
-#ifdef __clang__
-#  pragma clang diagnostic push
-#  pragma clang diagnostic ignored "-Wconversion"
-#  pragma clang diagnostic ignored "-Wold-style-cast"
-#elif defined(__GNUC__)
-#  pragma GCC diagnostic push
-#  pragma GCC diagnostic ignored "-Wconversion"
-#  pragma GCC diagnostic ignored "-Wsign-conversion"
-#  pragma GCC diagnostic ignored "-Wold-style-cast"
+#if defined(__clang__) && __clang_major__ >= 19
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
 #endif
 
-#include <boost/uuid/detail/sha1.hpp>
+#include <boost/core/lightweight_test.hpp>
 
-#ifdef __clang__
-#  pragma clang diagnostic pop
-#elif defined(__GNUC__)
-#  pragma GCC diagnostic pop
+#if defined(__clang__) && __clang_major__ >= 19
+#pragma clang diagnostic pop
 #endif
 
 #include <random>
@@ -34,53 +24,41 @@
 #include <cstdlib>
 #include <ctime>
 #include <cstring>
+#include <cstdint>
+#include <string>
 
-auto get_boost_uuid_result(const char* str, size_t length)
-{
-    unsigned char digest[20];
-    boost::uuids::detail::sha1 hasher;
-    hasher.process_bytes(str, length);
-    hasher.get_digest(digest);
+using std::byte;
 
-    std::array<unsigned char, 20> return_array {};
-    for (std::size_t i {}; i < 20U; ++i)
-    {
-        return_array[i] = digest[i];
-    }
-
-    return return_array;
-}
-
-constexpr std::array<std::tuple<const char*, std::array<uint16_t, 20>>, 7> test_values =
+const std::array<std::tuple<std::string, std::array<uint16_t, 20>>, 7> test_values =
 {
     // Start with the sample hashes from wiki
-    std::make_tuple("The quick brown fox jumps over the lazy dog",
+    std::make_tuple(std::string{"The quick brown fox jumps over the lazy dog"},
                     std::array<std::uint16_t, 20>{0x2f, 0xd4, 0xe1, 0xc6, 0x7a, 0x2d, 0x28, 0xfc, 0xed, 0x84,
                                                   0x9e, 0xe1, 0xbb, 0x76, 0xe7, 0x39, 0x1b, 0x93, 0xeb, 0x12}),
-    std::make_tuple("The quick brown fox jumps over the lazy cog",
+    std::make_tuple(std::string{"The quick brown fox jumps over the lazy cog"},
                     std::array<std::uint16_t, 20>{0xde, 0x9f, 0x2c, 0x7f, 0xd2, 0x5e, 0x1b, 0x3a, 0xfa, 0xd3,
                                                   0xe8, 0x5a, 0x0b, 0xd1, 0x7d, 0x9b, 0x10, 0x0d, 0xb4, 0xb3}),
-    std::make_tuple("",
+    std::make_tuple(std::string{""},
                     std::array<std::uint16_t, 20>{0xda, 0x39, 0xa3, 0xee, 0x5e, 0x6b, 0x4b, 0x0d, 0x32, 0x55,
                                                   0xbf, 0xef, 0x95, 0x60, 0x18, 0x90, 0xaf, 0xd8, 0x07, 0x09}),
     // Now the ones from the RFC
-    std::make_tuple("abc",
+    std::make_tuple(std::string{"abc"},
                     std::array<std::uint16_t, 20>{0xA9, 0x99, 0x3E, 0x36, 0x47, 0x06, 0x81, 0x6A, 0xBA, 0x3E,
                                                   0x25, 0x71, 0x78, 0x50, 0xC2, 0x6C, 0x9C, 0xD0, 0xD8, 0x9D}),
 
-    std::make_tuple("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+    std::make_tuple(std::string{"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"},
                     std::array<std::uint16_t, 20>{0x84, 0x98, 0x3E, 0x44, 0x1C, 0x3B, 0xD2, 0x6E, 0xBA, 0xAE,
                                                   0x4A, 0xA1, 0xF9, 0x51, 0x29, 0xE5, 0xE5, 0x46, 0x70, 0xF1}),
 
-    std::make_tuple("a",
+    std::make_tuple(std::string{"a"},
                     std::array<std::uint16_t, 20>{0x86, 0xf7, 0xe4, 0x37, 0xfa, 0xa5, 0xa7, 0xfc, 0xe1, 0x5d,
                                                   0x1d, 0xdc, 0xb9, 0xea, 0xea, 0xea, 0x37, 0x76, 0x67, 0xb8}),
 
-    std::make_tuple("0123456701234567012345670123456701234567012345670123456701234567",
+    std::make_tuple(std::string{"0123456701234567012345670123456701234567012345670123456701234567"},
                     std::array<std::uint16_t, 20>{0xe0, 0xc0, 0x94, 0xe8, 0x67, 0xef, 0x46, 0xc3, 0x50, 0xef,
                                                   0x54, 0xa7, 0xf5, 0x9d, 0xd6, 0x0b, 0xed, 0x92, 0xae, 0x83}),
 };
-
+/*
 void basic_tests()
 {
     for (const auto& test_value : test_values)
@@ -89,7 +67,7 @@ void basic_tests()
         const auto valid_result {std::get<1>(test_value)};
         for (std::size_t i {}; i < message_result.size(); ++i)
         {
-            if (!BOOST_TEST_EQ(message_result[i], valid_result[i]))
+            if (!BOOST_TEST(message_result[i] == static_cast<byte>(valid_result[i])))
             {
                 // LCOV_EXCL_START
                 std::cerr << "Failure with: " << std::get<0>(test_value) << '\n';
@@ -109,7 +87,7 @@ void string_test()
         const auto valid_result {std::get<1>(test_value)};
         for (std::size_t i {}; i < message_result.size(); ++i)
         {
-            if (!BOOST_TEST_EQ(message_result[i], valid_result[i]))
+            if (!BOOST_TEST(message_result[i] == static_cast<byte>(valid_result[i])))
             {
                 // LCOV_EXCL_START
                 std::cerr << "Failure with: " << std::get<0>(test_value) << '\n';
@@ -146,36 +124,37 @@ void string_view_test()
 void bad_input()
 {
     const auto null_message {boost::crypt::sha1(static_cast<const char*>(nullptr))};
-    BOOST_TEST_EQ(null_message[0], 0x0);
-    BOOST_TEST_EQ(null_message[1], 0x0);
-    BOOST_TEST_EQ(null_message[2], 0x0);
-    BOOST_TEST_EQ(null_message[3], 0x0);
+    BOOST_TEST(null_message[0] == byte{});
+    BOOST_TEST(null_message[1] == byte{});
+    BOOST_TEST(null_message[2] == byte{});
+    BOOST_TEST(null_message[3] == byte{});
 
     const auto null_message_len {boost::crypt::sha1(static_cast<const char*>(nullptr), 100)};
-    BOOST_TEST_EQ(null_message_len[0], 0x0);
-    BOOST_TEST_EQ(null_message_len[1], 0x0);
-    BOOST_TEST_EQ(null_message_len[2], 0x0);
-    BOOST_TEST_EQ(null_message_len[3], 0x0);
+    BOOST_TEST(null_message_len[0] == byte{});
+    BOOST_TEST(null_message_len[1] == byte{});
+    BOOST_TEST(null_message_len[2] == byte{});
+    BOOST_TEST(null_message_len[3] == byte{});
 
     const auto unsigned_null_message {boost::crypt::sha1(static_cast<const std::uint8_t*>(nullptr))};
-    BOOST_TEST_EQ(unsigned_null_message[0], 0x0);
-    BOOST_TEST_EQ(unsigned_null_message[1], 0x0);
-    BOOST_TEST_EQ(unsigned_null_message[2], 0x0);
-    BOOST_TEST_EQ(unsigned_null_message[3], 0x0);
+    BOOST_TEST(unsigned_null_message[0] == byte{});
+    BOOST_TEST(unsigned_null_message[1] == byte{});
+    BOOST_TEST(unsigned_null_message[2] == byte{});
+    BOOST_TEST(unsigned_null_message[3] == byte{});
 
     const auto unsigned_null_message_len {boost::crypt::sha1(static_cast<const std::uint8_t*>(nullptr), 100)};
-    BOOST_TEST_EQ(unsigned_null_message_len[0], 0x0);
-    BOOST_TEST_EQ(unsigned_null_message_len[1], 0x0);
-    BOOST_TEST_EQ(unsigned_null_message_len[2], 0x0);
-    BOOST_TEST_EQ(unsigned_null_message_len[3], 0x0);
+    BOOST_TEST(unsigned_null_message_len[0] == byte{});
+    BOOST_TEST(unsigned_null_message_len[1] == byte{});
+    BOOST_TEST(unsigned_null_message_len[2] == byte{});
+    BOOST_TEST(unsigned_null_message_len[3] == byte{});
 
     std::string test_str {"Test string"};
     const auto reveresed_input {boost::crypt::detail::sha1(test_str.end(), test_str.begin())};
-    BOOST_TEST_EQ(reveresed_input[0], 0x0);
-    BOOST_TEST_EQ(reveresed_input[1], 0x0);
-    BOOST_TEST_EQ(reveresed_input[2], 0x0);
-    BOOST_TEST_EQ(reveresed_input[3], 0x0);
+    BOOST_TEST_EQ(reveresed_input[0], byte{});
+    BOOST_TEST_EQ(reveresed_input[1], byte{});
+    BOOST_TEST_EQ(reveresed_input[2], byte{});
+    BOOST_TEST_EQ(reveresed_input[3], byte{});
 }
+*/
 
 void test_class()
 {
@@ -184,13 +163,13 @@ void test_class()
     for (const auto& test_value : test_values)
     {
         const auto msg {std::get<0>(test_value)};
-        hasher.process_bytes(msg, std::strlen(msg));
+        hasher.process_bytes(msg);
         const auto message_result {hasher.get_digest()};
 
         const auto valid_result {std::get<1>(test_value)};
         for (std::size_t i {}; i < message_result.size(); ++i)
         {
-            if (!BOOST_TEST_EQ(message_result[i], valid_result[i]))
+            if (!BOOST_TEST(message_result[i] == static_cast<byte>(valid_result[i])))
             {
                 // LCOV_EXCL_START
                 std::cerr << "Failure with: " << std::get<0>(test_value) << '\n';
@@ -202,7 +181,7 @@ void test_class()
         hasher.init();
     }
 }
-
+/*
 template <typename T>
 void test_random_values()
 {
@@ -474,15 +453,17 @@ void test_span()
     #endif // BOOST_CRYPT_HAS_SPAN
 }
 // LCOV_EXCL_STOP
-
+*/
 int main()
 {
+    /*
     basic_tests();
     string_test();
     string_view_test();
     bad_input();
+     */
     test_class();
-
+    /*
     test_random_values<char>();
     test_random_piecewise_values<char>();
 
@@ -503,6 +484,6 @@ int main()
     test_invalid_state();
 
     test_span();
-
+    */
     return boost::report_errors();
 }
