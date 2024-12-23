8332dfea799e82969e193592875680aa4790a64b
Matt Borland <matt@mattborland.com>
2024-12-23 16:35:59 -0500

    Fix bad impl of compat utils

--- include/boost/crypt2/detail/compat.hpp
+++ include/boost/crypt2/detail/compat.hpp
@@ -20,10 +20,7 @@
 #include <cstdint>
 #include <cstddef>
 #include <utility>
-
-namespace boost::crypt::compat {
-    namespace detail = std;
-}
+#include <bit>
 
 #elif defined(BOOST_CRYPT_HAS_CUDA)
 
@@ -35,110 +32,182 @@ namespace boost::crypt::compat {
 #include <cuda/std/concepts>
 #include <cuda/std/ranges>
 #include <cuda/std/utility>
-
-namespace boost::crypt::compat {
-    namespace detail = cuda::std;
-}
+#include <cuda/std/bit>
 
 #endif
 
 namespace boost::crypt::compat {
 
 // Fixed width types
-using size_t = detail::size_t;
-using uint32_t = detail::uint32_t;
+#ifdef BOOST_CRYPT_HAS_CUDA
+using size_t = cuda::std::size_t;
+using uint32_t = cuda::std::uint32_t;
+#else
+using size_t = std::size_t;
+using uint32_t = std::uint32_t;
+#endif
 
 // Arrays and spans
 template <typename T, compat::size_t N>
-using array = detail::array<T, N>;
+#ifdef BOOST_CRYPT_HAS_CUDA
+using array = cuda::std::array<T, N>;
+#else
+using array = std::array<T, N>;
+#endif
 
-template<typename T, compat::size_t Extent = detail::dynamic_extent>
-using span = detail::span<T, Extent>;
+#ifdef BOOST_CRYPT_HAS_CUDA
+template<typename T, cuda::std::size_t Extent = cuda::std::dynamic_extent>
+using span = cuda::std::span<T, Extent>;
+#else
+template<typename T, std::size_t Extent = std::dynamic_extent>
+using span = std::span<T, Extent>;
+#endif
 
 // Byte and friends
-using byte = detail::byte;
+#ifdef BOOST_CRYPT_HAS_CUDA
+using byte = cuda::std::byte;
+#else
+using byte = std::byte;
+#endif
 
 template <typename T>
-constexpr auto as_bytes(span<T> s) noexcept
+BOOST_CRYPT_GPU_ENABLED constexpr auto as_bytes(span<T> s) noexcept
 {
-    return detail::as_bytes(s);
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    return cuda::std::as_bytes(s);
+    #else
+    return std::as_bytes(s);
+    #endif
 }
 
 template <typename T>
-constexpr auto as_writable_bytes(span<T> s) noexcept
+BOOST_CRYPT_GPU_ENABLED constexpr auto as_writable_bytes(span<T> s) noexcept
 {
-    return detail::as_writable_bytes(s);
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    return cuda::std::as_writable_bytes(s);
+    #else
+    return std::as_writable_bytes(s);
+    #endif
 }
 
 // Type traits
-using true_type = detail::true_type;
-using false_type = detail::false_type;
+#ifdef BOOST_CRYPT_HAS_CUDA
+using true_type = cuda::std::true_type;
+using false_type = cuda::std::false_type;
+#else
+using true_type = std::true_type;
+using false_type = std::false_type;
+#endif
 
 template <typename T>
-inline constexpr bool is_trivially_copyable_v = detail::is_trivially_copyable_v<T>;
+inline constexpr bool is_trivially_copyable_v =
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    cuda::std::is_trivially_copyable_v<T>;
+    #else
+    std::is_trivially_copyable_v<T>;
+    #endif
+
+template <typename T>
+using remove_reference_t =
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    cuda::std::remove_reference_t<T>;
+    #else
+    std::remove_reference_t<T>;
+    #endif
+
+template <typename T>
+using remove_cvref_t =
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    cuda::std::remove_cv_t<cuda::std::remove_reference_t<T>>;
+    #else
+    std::remove_cv_t<std::remove_reference_t<T>>;
+    #endif
 
 // Ranges concepts and utilities
 template <typename R>
-concept sized_range = detail::ranges::sized_range<R>;
+concept sized_range =
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    cuda::std::ranges::sized_range<R>;
+    #else
+    std::ranges::sized_range<R>;
+    #endif
 
 template <typename R, typename T>
-concept output_range = detail::ranges::output_range<R, T>;
+concept output_range =
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    cuda::std::ranges::output_range<R, T>;
+    #else
+    std::ranges::output_range<R, T>;
+    #endif
 
 template <typename R>
-using range_value_t = detail::ranges::range_value_t<R>;
+using range_value_t =
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    cuda::std::ranges::range_value_t<R>;
+    #else
+    std::ranges::range_value_t<R>;
+    #endif
 
 // Utilities
 template <typename T>
-constexpr auto forward(detail::remove_reference_t<T>& t) noexcept -> T&&
+BOOST_CRYPT_GPU_ENABLED constexpr auto forward(remove_reference_t<T>& t) noexcept -> T&&
 {
-    return detail::forward<T>(t);
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    return cuda::std::forward<T>(t);
+    #else
+    return std::forward<T>(t);
+    #endif
 }
 
 template <typename T>
-constexpr auto forward(detail::remove_reference_t<T>&& t) noexcept -> T&&
+BOOST_CRYPT_GPU_ENABLED constexpr auto forward(remove_reference_t<T>&& t) noexcept -> T&&
 {
-    return detail::forward<T>(detail::move(t));
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    return cuda::std::forward<T>(cuda::std::move(t));
+    #else
+    return std::forward<T>(std::move(t));
+    #endif
 }
 
 // Helper functions
-template<typename T>
-struct is_span : detail::false_type {};
+template <typename T>
+struct is_span : false_type {};
 
-template<typename T, size_t Extent>
-struct is_span<detail::span<T, Extent>> : detail::true_type {};
+template <typename T, size_t Extent>
+struct is_span<span<T, Extent>> : true_type {};
 
-template<typename T, size_t Extent>
-struct is_span<const detail::span<T, Extent>> : detail::true_type {};
+template <typename T, size_t Extent>
+struct is_span<const span<T, Extent>> : true_type {};
 
 // Helper variable template
 template<typename T>
 inline constexpr bool is_span_v = is_span<T>::value;
 
-// Add remove_cvref type trait if not available
-template<typename T>
-using remove_cvref_t = detail::remove_cv_t<detail::remove_reference_t<T>>;
-
 template <typename R>
-constexpr auto make_span(R&& r)
+BOOST_CRYPT_GPU_ENABLED constexpr auto make_span(R&& r)
 {
-    if constexpr (is_span_v<detail::remove_cvref_t<R>>)
+    if constexpr (is_span_v<remove_cvref_t<R>>)
     {
-        return detail::forward<R>(r);
+        #ifdef BOOST_CRYPT_HAS_CUDA
+        return cuda::std::forward<R>(r);
+        #else
+        return std::forward<R>(r);
+        #endif
     }
     else
     {
         #ifdef BOOST_CRYPT_HAS_CUDA
-        return cuda::std::span(detail::forward<R>(r));
+        return cuda::std::span(cuda::std::forward<R>(r));
         #else
-        return std::span(detail::forward<R>(r));
+        return std::span(std::forward<R>(r));
         #endif
     }
 }
 
 template <typename R>
-constexpr auto make_span(R& r)
+BOOST_CRYPT_GPU_ENABLED constexpr auto make_span(R& r)
 {
-    if constexpr (is_span_v<detail::remove_cvref_t<R>>)
+    if constexpr (is_span_v<remove_cvref_t<R>>)
     {
         return r;
     }
@@ -150,6 +219,17 @@ constexpr auto make_span(R& r)
         return std::span(r);
         #endif
     }
+}
+
+// bit
+template <typename T>
+BOOST_CRYPT_GPU_ENABLED constexpr auto rotl(T val, T shift) noexcept
+{
+    #ifdef BOOST_CRYPT_HAS_CUDA
+    return cuda::std::rotl(val, shift);
+    #else
+    return std::rotl(val, shift);
+    #endif
 }
 
 } // namespace boost::crypt::compat
