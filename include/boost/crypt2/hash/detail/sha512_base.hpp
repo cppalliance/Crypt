78110830a0a480b015dff8ab47175ad9eeddca44
Matt Borland <matt@mattborland.com>
2025-01-09 15:58:17 -0500

    Implement process message block

--- include/boost/crypt2/hash/detail/sha512_base.hpp
+++ include/boost/crypt2/hash/detail/sha512_base.hpp
@@ -168,8 +168,10 @@ BOOST_CRYPT_GPU_ENABLED_CONSTEXPR auto sha512_base<digest_size>::init() noexcept
 namespace sha512_detail {
     
 #ifndef BOOST_CRYPT_HAS_CUDA
-    
-inline constexpr std::array<std::uint64_t, 80U> sha512_k {
+
+// On the host device we prefer this array to be static,
+// but in a CUDA environment we move it into the function to make it available to host and device
+inline constexpr compat::array<compat::uint64_t, 80U> sha512_k {
     0x428A2F98D728AE22ULL, 0x7137449123EF65CDULL, 0xB5C0FBCFEC4D3B2FULL,
     0xE9B5DBA58189DBBCULL, 0x3956C25BF348B538ULL, 0x59F111F1B605D019ULL,
     0x923F82A4AF194F9BULL, 0xAB1C5ED5DA6D8118ULL, 0xD807AA98A3030242ULL,
@@ -237,6 +239,105 @@ BOOST_CRYPT_GPU_ENABLED_CONSTEXPR auto sha_maj(const compat::uint64_t x,
 }
         
 } // namespace sha512_detail
+
+template <compat::size_t digest_size>
+BOOST_CRYPT_GPU_ENABLED_CONSTEXPR auto sha512_base<digest_size>::process_message_block() noexcept -> void
+{
+    #ifdef BOOST_CRYPT_HAS_CUDA
+
+    constexpr compat::array<compat::uint64_t, 80U> sha512_k = {
+            0x428A2F98D728AE22ULL, 0x7137449123EF65CDULL, 0xB5C0FBCFEC4D3B2FULL,
+            0xE9B5DBA58189DBBCULL, 0x3956C25BF348B538ULL, 0x59F111F1B605D019ULL,
+            0x923F82A4AF194F9BULL, 0xAB1C5ED5DA6D8118ULL, 0xD807AA98A3030242ULL,
+            0x12835B0145706FBEULL, 0x243185BE4EE4B28CULL, 0x550C7DC3D5FFB4E2ULL,
+            0x72BE5D74F27B896FULL, 0x80DEB1FE3B1696B1ULL, 0x9BDC06A725C71235ULL,
+            0xC19BF174CF692694ULL, 0xE49B69C19EF14AD2ULL, 0xEFBE4786384F25E3ULL,
+            0x0FC19DC68B8CD5B5ULL, 0x240CA1CC77AC9C65ULL, 0x2DE92C6F592B0275ULL,
+            0x4A7484AA6EA6E483ULL, 0x5CB0A9DCBD41FBD4ULL, 0x76F988DA831153B5ULL,
+            0x983E5152EE66DFABULL, 0xA831C66D2DB43210ULL, 0xB00327C898FB213FULL,
+            0xBF597FC7BEEF0EE4ULL, 0xC6E00BF33DA88FC2ULL, 0xD5A79147930AA725ULL,
+            0x06CA6351E003826FULL, 0x142929670A0E6E70ULL, 0x27B70A8546D22FFCULL,
+            0x2E1B21385C26C926ULL, 0x4D2C6DFC5AC42AEDULL, 0x53380D139D95B3DFULL,
+            0x650A73548BAF63DEULL, 0x766A0ABB3C77B2A8ULL, 0x81C2C92E47EDAEE6ULL,
+            0x92722C851482353BULL, 0xA2BFE8A14CF10364ULL, 0xA81A664BBC423001ULL,
+            0xC24B8B70D0F89791ULL, 0xC76C51A30654BE30ULL, 0xD192E819D6EF5218ULL,
+            0xD69906245565A910ULL, 0xF40E35855771202AULL, 0x106AA07032BBD1B8ULL,
+            0x19A4C116B8D2D0C8ULL, 0x1E376C085141AB53ULL, 0x2748774CDF8EEB99ULL,
+            0x34B0BCB5E19B48A8ULL, 0x391C0CB3C5C95A63ULL, 0x4ED8AA4AE3418ACBULL,
+            0x5B9CCA4F7763E373ULL, 0x682E6FF3D6B2B8A3ULL, 0x748F82EE5DEFB2FCULL,
+            0x78A5636F43172F60ULL, 0x84C87814A1F0AB72ULL, 0x8CC702081A6439ECULL,
+            0x90BEFFFA23631E28ULL, 0xA4506CEBDE82BDE9ULL, 0xBEF9A3F7B2C67915ULL,
+            0xC67178F2E372532BULL, 0xCA273ECEEA26619CULL, 0xD186B8C721C0C207ULL,
+            0xEADA7DD6CDE0EB1EULL, 0xF57D4F7FEE6ED178ULL, 0x06F067AA72176FBAULL,
+            0x0A637DC5A2C898A6ULL, 0x113F9804BEF90DAEULL, 0x1B710B35131C471BULL,
+            0x28DB77F523047D84ULL, 0x32CAAB7B40C72493ULL, 0x3C9EBE0A15C9BEBCULL,
+            0x431D67C49C100D4CULL, 0x4CC5D4BECB3E42B6ULL, 0x597F299CFC657E2AULL,
+            0x5FCB6FAB3AD6FAECULL, 0x6C44198C4A475817ULL
+    };
+
+    #endif
+    
+    using namespace sha512_detail;
+    
+    compat::array<compat::uint64_t, 80U> W {};
+
+    // Init the first 16 words of W
+    static_assert(8U * 16U == buffer_.size());
+    for (compat::size_t i {}; i < 16U; ++i)
+    {
+        W[i] = (static_cast<compat::uint64_t>(buffer_[i * 8U]) << 56U) |
+               (static_cast<compat::uint64_t>(buffer_[i * 8U + 1U]) << 48U) |
+               (static_cast<compat::uint64_t>(buffer_[i * 8U + 2U]) << 40U) |
+               (static_cast<compat::uint64_t>(buffer_[i * 8U + 3U]) << 32U) |
+               (static_cast<compat::uint64_t>(buffer_[i * 8U + 4U]) << 24U) |
+               (static_cast<compat::uint64_t>(buffer_[i * 8U + 5U]) << 16U) |
+               (static_cast<compat::uint64_t>(buffer_[i * 8U + 6U]) << 8U) |
+               (static_cast<compat::uint64_t>(buffer_[i * 8U + 7U]));
+    }
+
+    // Init the last 64
+    for (compat::size_t i {16U}; i < W.size(); ++i)
+    {
+        W[i] = little_sigma1(W[i - 2U])  + W[i - 7U] +
+               little_sigma0(W[i - 15U]) + W[i - 16U];
+    }
+
+    auto A {intermediate_hash_[0]};
+    auto B {intermediate_hash_[1]};
+    auto C {intermediate_hash_[2]};
+    auto D {intermediate_hash_[3]};
+    auto E {intermediate_hash_[4]};
+    auto F {intermediate_hash_[5]};
+    auto G {intermediate_hash_[6]};
+    auto H {intermediate_hash_[7]};
+
+    static_assert(sha512_k.size() == W.size());
+    for (compat::size_t i {}; i < W.size(); ++i)
+    {
+        const auto temp1 {H + big_sigma1(E) + sha_ch(E, F, G) + sha512_k[i] + W[i]};
+        const auto temp2 {big_sigma0(A) + sha_maj(A, B, C)};
+        H = G;
+        G = F;
+        F = E;
+        E = D + temp1;
+        D = C;
+        C = B;
+        B = A;
+        A = temp1 + temp2;
+    }
+
+    intermediate_hash_[0] += A;
+    intermediate_hash_[1] += B;
+    intermediate_hash_[2] += C;
+    intermediate_hash_[3] += D;
+    intermediate_hash_[4] += E;
+    intermediate_hash_[5] += F;
+    intermediate_hash_[6] += G;
+    intermediate_hash_[7] += H;
+
+    // Reset the buffer index
+    buffer_index_ = 0U;
+}
 
 } // namespace boost::crypt::hash_detail
 
