ae8172ade198bca6483475c0419b5e58e4601dd0
Matt Borland <matt@mattborland.com>
2025-01-09 17:25:35 -0500

    Update test harness

--- test/test_sha256.cpp
+++ test/test_sha256.cpp
@@ -250,37 +250,6 @@ void files_test()
     BOOST_TEST_THROWS(boost::crypt::sha256_file(bad_path), std::runtime_error);
 }
 
-// This ends up being completely calculated in a constexpr fashion so Codecov complains
-// LCOV_EXCL_START
-void test_span()
-{
-    #ifdef BOOST_CRYPT_HAS_SPAN
-
-    // "abc" in hex
-    const std::byte vals[] = {std::byte{0x61}, std::byte{0x62}, std::byte{0x63}};
-    std::span<const std::byte> byte_span {vals};
-    const auto expected_res = std::array<std::uint16_t, 32>{0xba, 0x78, 0x16, 0xbf, 0x8f, 0x01, 0xcf, 0xea, 0x41, 0x41, 0x40, 0xde, 0x5d, 0xae, 0x22, 0x23, 0xb0, 0x03, 0x61, 0xa3, 0x96, 0x17, 0x7a, 0x9c, 0xb4, 0x10, 0xff, 0x61, 0xf2, 0x00, 0x15, 0xad};
-    const auto res = boost::crypt::sha256(byte_span);
-
-    for (std::size_t i {}; i < res.size(); ++i)
-    {
-        BOOST_TEST_EQ(res[i], expected_res[i]);
-    }
-
-    boost::crypt::sha256_hasher hasher;
-    auto current_state = hasher.process_bytes(byte_span);
-    BOOST_TEST(current_state == boost::crypt::state::success);
-    const auto res_2 = hasher.get_digest();
-
-    for (std::size_t i {}; i < res.size(); ++i)
-    {
-        BOOST_TEST_EQ(res_2[i], expected_res[i]);
-    }
-
-    #endif // BOOST_CRYPT_HAS_SPAN
-}
-// LCOV_EXCL_STOP
-
 consteval bool immediate_test()
 {
     constexpr std::array<std::byte, 3> vals = {std::byte{0x61}, std::byte{0x62}, std::byte{0x63}};
