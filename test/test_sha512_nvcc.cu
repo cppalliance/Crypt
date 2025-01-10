8c1cce51b68f794c25467d9f66a1d26e71864334
Matt Borland <matt@mattborland.com>
2025-01-10 12:43:54 -0500

    Add sha512 CUDA testing

--- test/test_sha512_nvcc.cu
+++ test/test_sha512_nvcc.cu
@@ -3,7 +3,8 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/crypt/hash/sha512.hpp>
+#include <cuda_runtime.h>
+#include <boost/crypt2/hash/sha512.hpp>
 #include "cuda_managed_ptr.hpp"
 #include "stopwatch.hpp"
 #include "generate_random_strings.hpp"
@@ -11,10 +12,9 @@
 #include <iomanip>
 #include <exception>
 #include <memory>
+#include <span>
 
-#include <cuda_runtime.h>
-
-using digest_type = boost::crypt::sha512_hasher::return_type;
+using digest_type = cuda::std::array<cuda::std::byte, 20>;
 
 // The kernel function
 __global__ void cuda_test(char** in, digest_type* out, int numElements)
@@ -23,7 +23,8 @@ __global__ void cuda_test(char** in, digest_type* out, int numElements)
 
     if (i < numElements)
     {
-        out[i] = boost::crypt::sha512(in[i]);
+        auto in_span {cuda::std::span(in[i], 64)};
+        out[i] = boost::crypt::sha512(in_span).value();
     }
 }
 
@@ -59,7 +60,7 @@ int main()
 
         // Launch the Vector Add CUDA Kernel
         int threadsPerBlock = 256;
-        int blocksPerGrid =(numElements + threadsPerBlock - 1) / threadsPerBlock;
+        int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
         std::cout << "CUDA kernel launch with " << blocksPerGrid << " blocks of " << threadsPerBlock << " threads" << std::endl;
 
         watch w;
@@ -80,7 +81,8 @@ int main()
         w.reset();
         for(int i = 0; i < numElements; ++i)
         {
-           results.emplace_back(boost::crypt::sha512(input_vector1[i]));
+            std::span<char> in(input_vector1[i], elementSize);
+            results.emplace_back(boost::crypt::sha512(in).value());
         }
         double t = w.elapsed();
 
