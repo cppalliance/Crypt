71ee8e8e02c1235956ce707207d0ba128d6c5e10
Matt Borland <matt@mattborland.com>
2025-01-14 10:49:43 -0500

    Add testing of additional output paths

--- test/test_sha1.cpp
+++ test/test_sha1.cpp
@@ -153,6 +153,25 @@ void test_class()
     BOOST_TEST(hasher.process_bytes(bad_update_msg) == boost::crypt::state::state_error);
     BOOST_TEST(hasher.finalize() == boost::crypt::state::state_error);
     BOOST_TEST(hasher.get_digest().error() == boost::crypt::state::state_error);
+
+    // Bad return value size
+    std::array<std::byte, 5U> bad_container {};
+    hasher.init();
+    hasher.process_bytes(bad_update_msg);
+    const auto array_return1 {hasher.get_digest(bad_container)};
+    BOOST_TEST(array_return1 == boost::crypt::state::state_error);
+    hasher.finalize();
+    const auto array_return2 {hasher.get_digest(bad_container)};
+    BOOST_TEST(array_return2 == boost::crypt::state::insufficient_output_length);
+
+    std::span<std::byte, 5U> bad_container_span {bad_container};
+    hasher.init();
+    hasher.process_bytes(bad_update_msg);
+    const auto array_return3 {hasher.get_digest(bad_container_span)};
+    BOOST_TEST(array_return3 == boost::crypt::state::state_error);
+    hasher.finalize();
+    const auto array_return4 {hasher.get_digest(bad_container_span)};
+    BOOST_TEST(array_return4 == boost::crypt::state::insufficient_output_length);
 }
 
 template <typename T>
