b062635d9f8892b3174c1b6802b404a818c4cd07
Matt Borland <matt@mattborland.com>
2025-01-14 09:02:46 -0500

    Add SHA3-384 impl

--- include/boost/crypt2/hash/sha3_384.hpp
+++ include/boost/crypt2/hash/sha3_384.hpp
@@ -2,8 +2,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#ifndef BOOST_CRYPT2_HASH_SHA3_512_HPP
-#define BOOST_CRYPT2_HASH_SHA3_512_HPP
+#ifndef BOOST_CRYPT2_HASH_SHA3_384_HPP
+#define BOOST_CRYPT2_HASH_SHA3_384_HPP
 
 #include <boost/crypt2/hash/detail/sha3_base.hpp>
 #include <boost/crypt2/hash/detail/hash_file.hpp>
@@ -12,13 +12,13 @@
 
 namespace boost::crypt {
 
-BOOST_CRYPT_EXPORT using sha3_512_hasher = hash_detail::sha3_base<64U>;
+BOOST_CRYPT_EXPORT using sha3_384_hasher = hash_detail::sha3_base<48U>;
 
 // One shot functions
 [[nodiscard]] BOOST_CRYPT_EXPORT BOOST_CRYPT_GPU_ENABLED_CONSTEXPR
-auto sha3_512(compat::span<const compat::byte> data) noexcept -> compat::expected<sha3_512_hasher::return_type, state>
+auto sha3_384(compat::span<const compat::byte> data) noexcept -> compat::expected<sha3_384_hasher::return_type, state>
 {
-    sha3_512_hasher hasher;
+    sha3_384_hasher hasher;
     hasher.process_bytes(data);
     hasher.finalize();
     return hasher.get_digest();
@@ -26,9 +26,9 @@ auto sha3_512(compat::span<const compat::byte> data) noexcept -> compat::expecte
 
 template <compat::sized_range SizedRange>
 [[nodiscard]] BOOST_CRYPT_EXPORT BOOST_CRYPT_GPU_ENABLED
-auto sha3_512(SizedRange&& data) noexcept -> compat::expected<sha3_512_hasher::return_type, state>
+auto sha3_384(SizedRange&& data) noexcept -> compat::expected<sha3_384_hasher::return_type, state>
 {
-    sha3_512_hasher hasher;
+    sha3_384_hasher hasher;
     hasher.process_bytes(data);
     hasher.finalize();
     return hasher.get_digest();
@@ -38,13 +38,13 @@ auto sha3_512(SizedRange&& data) noexcept -> compat::expected<sha3_512_hasher::r
 
 template <concepts::file_system_path T>
 [[nodiscard]] BOOST_CRYPT_EXPORT
-auto sha3_512_file(const T& filepath) -> compat::expected<sha3_512_hasher::return_type, state>
+auto sha3_384_file(const T& filepath) -> compat::expected<sha3_384_hasher::return_type, state>
 {
-    return hash_detail::hash_file_impl<sha3_512_hasher>(filepath);
+    return hash_detail::hash_file_impl<sha3_384_hasher>(filepath);
 }
 
 #endif // BOOST_CRYPT_HAS_CUDA
 
 } // namespace boost::crypt
 
-#endif //BOOST_CRYPT2_HASH_SHA3_512_HPP
+#endif // BOOST_CRYPT2_HASH_SHA3_384_HPP
