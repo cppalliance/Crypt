cf86ba08a6fbd45a5897e71163bd7181aa6c0d19
Matt Borland <matt@mattborland.com>
2025-01-14 09:03:16 -0500

    Re-enable NIST CAVS testing

--- test/test_nist_cavs_sha3_384_short_long.cpp
+++ test/test_nist_cavs_sha3_384_short_long.cpp
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/crypt/hash/sha3_384.hpp>
+#include <boost/crypt2/hash/sha3_384.hpp>
 
 #include "test_nist_cavs_detail.hpp"
 
