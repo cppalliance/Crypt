116df514d920b09dddc3f7c86db40934c71bd99d
Matt Borland <matt@mattborland.com>
2025-01-16 15:27:28 -0500

    Reactivate all CAVS tests

--- test/test_nist_cavs_sha512_256_hmac.cpp
+++ test/test_nist_cavs_sha512_256_hmac.cpp
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/crypt/hash/sha512_256.hpp>
+#include <boost/crypt2/hash/sha512_256.hpp>
 
 #include "test_nist_cavs_detail.hpp"
 
