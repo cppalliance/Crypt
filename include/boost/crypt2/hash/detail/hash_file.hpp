5d8f9716b31d82b165eb5696029d292fab70d0c1
Matt Borland <matt@mattborland.com>
2025-01-17 09:19:26 -0500

    Add XOF file hashing

--- include/boost/crypt2/hash/detail/hash_file.hpp
+++ include/boost/crypt2/hash/detail/hash_file.hpp
@@ -13,6 +13,10 @@
 #include <boost/crypt2/detail/concepts.hpp>
 #include <boost/crypt2/detail/compat.hpp>
 
+#if !defined(BOOST_CRYPT_BUILD_MODULE)
+#include <limits>
+#endif
+
 namespace boost::crypt::hash_detail {
 
 // Error: the two-parameter std::span construction is unsafe as it can introduce mismatch between buffer size and the bound information [-Werror,-Wunsafe-buffer-usage-in-container]
@@ -49,6 +53,39 @@ template <typename HasherType, concepts::file_system_path T>
     return hasher.get_digest();
 }
 
+template <typename HasherType, concepts::file_system_path T>
+[[nodiscard]] auto hash_file_impl(const T& filepath, std::span<std::byte> out, std::size_t amount = std::numeric_limits<std::size_t>::max()) -> state
+{
+    if constexpr (std::is_pointer_v<std::remove_cvref_t<T>>)
+    {
+        if (filepath == nullptr)
+        {
+            throw std::runtime_error("Invalid file path");
+        }
+    }
+
+    detail::file_reader<HasherType::block_size> reader(filepath);
+    HasherType hasher;
+
+    while (!reader.eof())
+    {
+        const auto buffer_iter {reader.read_next_block()};
+        const auto len {reader.get_bytes_read()};
+        const auto buffer_span {std::span(buffer_iter, len)};
+        hasher.process_bytes(buffer_span);
+    }
+
+    hasher.finalize();
+
+    if (amount == std::numeric_limits<std::size_t>::max())
+    {
+        return hasher.get_digest(out);
+    }
+    else
+    {
+        return hasher.get_digest(out, amount);
+    }
+}
 
 #if defined(__clang__) && __clang_major__ >= 19
 #pragma clang diagnostic pop
