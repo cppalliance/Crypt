c1a29d52888988aa7928858cb8676f727e0dddc7
Matt Borland <matt@mattborland.com>
2025-01-17 15:25:11 -0500

    Implement hash_drbg

--- include/boost/crypt2/drbg/detail/hash_drbg.hpp
+++ include/boost/crypt2/drbg/detail/hash_drbg.hpp
@@ -9,6 +9,7 @@
 #include <boost/crypt2/detail/compat.hpp>
 #include <boost/crypt2/detail/concepts.hpp>
 #include <boost/crypt2/detail/clear_mem.hpp>
+#include <boost/crypt2/state.hpp>
 
 namespace boost::crypt::drbg_detail {
 
@@ -47,12 +48,75 @@ private:
     compat::uint64_t reseed_counter_ {};
     bool initialized_ {};
 
+    BOOST_CRYPT_GPU_ENABLED_CONSTEXPR auto hash_df(compat::uint32_t no_of_bits_to_return,
+                                                   compat::span<compat::byte> return_container,
+                                                   compat::span<const compat::byte> provided_data_1,
+                                                   compat::span<const compat::byte> provided_data_2,
+                                                   compat::span<const compat::byte> provided_data_3,
+                                                   compat::span<const compat::byte> provided_data_4) noexcept -> state;
+
 public:
 
     BOOST_CRYPT_GPU_ENABLED_CONSTEXPR hash_drbg() noexcept = default;
 
     BOOST_CRYPT_GPU_ENABLED_CONSTEXPR ~hash_drbg() noexcept;
 };
+
+template <typename HasherType, compat::size_t max_hasher_security, compat::size_t outlen, bool prediction_resistance>
+BOOST_CRYPT_GPU_ENABLED_CONSTEXPR auto hash_drbg<HasherType, max_hasher_security, outlen, prediction_resistance>::hash_df(
+        compat::uint32_t no_of_bits_to_return,
+        compat::span<compat::byte> return_container,
+        compat::span<const compat::byte> provided_data_1,
+        compat::span<const compat::byte> provided_data_2,
+        compat::span<const compat::byte> provided_data_3,
+        compat::span<const compat::byte> provided_data_4) noexcept -> state
+{
+    const auto no_of_bytes_to_return {(no_of_bits_to_return + 7U) / 8U};
+    const auto len {(no_of_bytes_to_return + outlen_bytes - 1U) / outlen_bytes};
+
+    if (len > 255U) [[unlikely]]
+    {
+        return state::requested_too_many_bits;
+    }
+    else if (return_container.size() < no_of_bytes_to_return) [[unlikely]]
+    {
+        return state::out_of_memory;
+    }
+
+    // The hash string concatenates the value of no_of_bits_to_return
+    const compat::array<compat::byte, 4U> bits_to_return_array {
+        static_cast<compat::byte>((no_of_bits_to_return >> 24) & 0xFF),
+        static_cast<compat::byte>((no_of_bits_to_return >> 16) & 0xFF),
+        static_cast<compat::byte>((no_of_bits_to_return >> 8) & 0xFF),
+        static_cast<compat::byte>(no_of_bits_to_return & 0xFF)
+    };
+    const compat::span<const compat::byte, 4U> bits_to_return_span {bits_to_return_array};
+
+    // See 10.3.1
+    // temp = temp || HASH(counter, no_of_bits_to_return || input_string)
+    compat::size_t offset {};
+    for (compat::size_t counter {1}; counter <= len; ++counter)
+    {
+        HasherType hasher;
+        hasher.process_byte(static_cast<compat::byte>(counter));
+        hasher.process_bytes(bits_to_return_span);
+        hasher.process_bytes(provided_data_1);
+        hasher.process_bytes(provided_data_2);
+        hasher.process_bytes(provided_data_3);
+        hasher.process_bytes(provided_data_4);
+        hasher.finalize();
+        const auto return_val {hasher.get_digest()};
+
+        BOOST_CRYPT_ASSERT(return_val.has_value());
+
+        const auto return_val_array {return_val.value()};
+
+        for (compat::size_t i {}; i < return_val_array.size() && offset < no_of_bytes_to_return; ++i)
+        {
+            return_container[offset++] = return_val[i];
+        }
+    }
+}
 
 template <typename HasherType, compat::size_t max_hasher_security, compat::size_t outlen, bool prediction_resistance>
 BOOST_CRYPT_GPU_ENABLED_CONSTEXPR hash_drbg<HasherType, max_hasher_security, outlen, prediction_resistance>::~hash_drbg() noexcept
